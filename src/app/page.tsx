ea04fb89784a7ba9ab339a686af8cabec5b3674a
Jun-Murakami <contact@bucketrelay.com>
2025-09-19 05:11:20 +0900

    feat: ページの動的生成を強制し、再検証の設定を追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 import GitHubIcon from "@mui/icons-material/GitHub";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import MailOutlineIcon from "@mui/icons-material/MailOutline";
